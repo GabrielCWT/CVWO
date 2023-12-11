0f2969360e14393b932a0fbd60e065882d69feef
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-11 14:43:28 +0800

    Include Submit Button

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,5 +1,5 @@
 import MultiSelect from "../components/Form/MultiSelect";
-import { Box, Container, TextField } from "@mui/material";
+import { Box, Container, TextField, Button } from "@mui/material";
 import React from "react";
 
 const categories = ["Technology", "Sports", "Politics", "Entertainment", "Science", "Health"];
@@ -8,10 +8,13 @@ const CreatePost: React.FC = () => {
     return (
         <Container>
             <h1>{"Create Post"}</h1>
-            <Box component="form">
+            <Box component="form" sx={{ "& > :not(style)": { m: 1 }, display: "grid" }} method="post">
                 <TextField id="title" label="Title" variant="standard" required />
                 <MultiSelect label="Tags" options={categories} />
                 <TextField id="content" label="Content" variant="outlined" multiline fullWidth required />
+                <Button type="submit" variant="contained">
+                    Submit
+                </Button>
             </Box>
         </Container>
     );
