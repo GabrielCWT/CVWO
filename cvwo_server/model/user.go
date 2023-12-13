ad39a7b7bf00b0d72579162bacf41aa3a1c0ded4
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-13 15:09:44 +0800

    Implement creating of user

--- cvwo_server/model/user.go
+++ cvwo_server/model/user.go
@@ -1,9 +1,33 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"cvwo_server/database"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"html"
+	"strings"
+)
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique;not null" json:"username"`
-	Password string `gorm:"not null" json:"-"`
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Password string `gorm:"size:255;not null;" json:"-"`
+}
+
+func (user *User) Save() (*User, error) {
+	err := database.Database.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return user, nil
+}
+
+func (user *User) BeforeSave(*gorm.DB) error{
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(passwordHash)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	return nil
 }
