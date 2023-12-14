22e2c40396bd45436920d81ce89a6fd7cc01b9a8
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-14 09:34:15 +0800

    Chang authInput to take form instead of json

--- cvwo_server/model/authenticationInput.go
+++ cvwo_server/model/authenticationInput.go
@@ -1,6 +1,6 @@
 package model
 
 type AuthenticationInput struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string `form:"username" binding:"required"`
+	Password string `form:"password" binding:"required"`
 }
