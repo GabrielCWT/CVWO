e5618b5cd039976319c15592121c3c83b207b914
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-14 21:51:15 +0800

    Implement signIn context

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import StyledThreadView from "./pages/StyledThreadView";
 import CreatePost from "./pages/CreatePost";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { blue, orange } from "@mui/material/colors";
+
+type CurrentUser = {
+    isSignedIn: boolean;
+    username: string;
+};
+
+type CurrentUserContextType = {
+    currentUser: CurrentUser;
+    setCurrentUser: React.Dispatch<React.SetStateAction<CurrentUser>>;
+};
 
 const theme = createTheme({
     palette: {
@@ -16,19 +26,27 @@ const theme = createTheme({
     },
 });
 
+export const CurrentUserContext = createContext<CurrentUserContextType>({
+    currentUser: { isSignedIn: false, username: "" },
+    setCurrentUser: () => {},
+});
+
 const App: React.FC = () => {
+    const [currentUser, setCurrentUser] = useState<CurrentUser>({ isSignedIn: false, username: "" });
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/test" element={<StyledThreadView />} />
-                        <Route path="/" element={<Home />} />
-                        <Route path="/create" element={<CreatePost />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </BrowserRouter>
+                <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/test" element={<StyledThreadView />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/create" element={<CreatePost />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/login" element={<Login />} />
+                        </Routes>
+                    </BrowserRouter>
+                </CurrentUserContext.Provider>
             </ThemeProvider>
         </div>
     );
