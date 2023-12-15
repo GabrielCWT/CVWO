deaaa7c2e426ef2cd2100d53a88009f49542b1c2
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-15 09:39:03 +0800

    Implement autologin with cookies

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import StyledThreadView from "./pages/StyledThreadView";
 import CreatePost from "./pages/CreatePost";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { blue, orange } from "@mui/material/colors";
+import axios from "axios";
 
 type CurrentUser = {
     isSignedIn: boolean;
@@ -33,6 +34,15 @@ export const CurrentUserContext = createContext<CurrentUserContextType>({
 
 const App: React.FC = () => {
     const [currentUser, setCurrentUser] = useState<CurrentUser>({ isSignedIn: false, username: "" });
+    useEffect(() => {
+        console.log("checking auth");
+        axios
+            .get("http://localhost:8000/auth/verify", { withCredentials: true })
+            .then((res) => {
+                setCurrentUser({ isSignedIn: true, username: res.data.data.username });
+            })
+            .catch(() => {});
+    }, []);
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import BasicThreadList from "../components/BasicThreadList";
-import React from "react";
+import { CurrentUserContext } from "../App";
+import React, { useContext } from "react";
 
 const Home: React.FC = () => {
+    const { currentUser } = useContext(CurrentUserContext);
     return (
         <>
+            {currentUser.isSignedIn && <h1>{`Welcome, ${currentUser.username}!`}</h1>}
             <h3>
                 {"Welcome to CVWO's sample react app! Here's a basic list of forum threads for you to experiment with."}
             </h3>
