102464c5515a141f3ece43d08825fa5af88279a5
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-15 23:05:47 +0800

    Implement get helper functions for posts

--- src/scripts/apiHelpers.tsx
+++ src/scripts/apiHelpers.tsx
@@ -4,3 +4,13 @@ export const getAllPosts = async () => {
     const res = await axios.get("http://localhost:8000/api/posts");
     return res.data.data;
 };
+
+export const getPostByCategory = async (category: string) => {
+    const res = await axios.get(`http://localhost:8000/api/posts/${category}`);
+    return res.data.data;
+};
+
+export const getPostByID = async (postID: string) => {
+    const res = await axios.get(`http://localhost:8000/api/posts/post/${postID}`);
+    return res.data.data;
+};
