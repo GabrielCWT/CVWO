f6c4ae4b4e03aac4ebbc0f5e571ebd69e5b198be
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-15 08:57:55 +0800

    Implement signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,19 +1,36 @@
-import { Box, Container, TextField, Button } from "@mui/material";
-import React from "react";
-import axios from "axios";
+import { CurrentUserContext } from "../App";
+import { Box, Container, TextField, Button, FormHelperText } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import axios, { AxiosError } from "axios";
 
-const Signup: React.FC = () => {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+const Login: React.FC = () => {
+    const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+    const [helperMessage, setHelperMessage] = useState<string>("");
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const data = new FormData(e.currentTarget);
         console.log({
             username: data.get("username"),
             password: data.get("password"),
         });
-        axios.post("http://localhost:8000/auth/signup", data).then((response) => {
-            console.log(response);
-        });
+        try {
+            const res = await axios.post("http://localhost:8000/auth/signup", data);
+            console.log(res);
+            setCurrentUser({ isSignedIn: true, username: data.get("username") as string });
+        } catch (err) {
+            const error = err as AxiosError;
+            const errorMessage = JSON.parse(error.request.response).error;
+            if (errorMessage === "Error validating password") {
+                setHelperMessage("User already exists");
+            } else {
+                setHelperMessage("Unknown error");
+            }
+        }
     };
+    if (currentUser.isSignedIn) {
+        return <Navigate to="/" />;
+    }
     return (
         <Container
             sx={{
@@ -25,6 +42,7 @@ const Signup: React.FC = () => {
             <Box component="form" sx={{ display: "grid", gap: 5 }} onSubmit={handleSubmit}>
                 <TextField id="username" name="username" label="Username" variant="standard" required autoFocus />
                 <TextField id="password" name="password" label="Password" variant="standard" required autoFocus />
+                <FormHelperText id="my-helper-text">{helperMessage}</FormHelperText>
                 <Button type="submit" variant="contained">
                     Submit
                 </Button>
@@ -33,4 +51,4 @@ const Signup: React.FC = () => {
     );
 };
 
-export default Signup;
+export default Login;
