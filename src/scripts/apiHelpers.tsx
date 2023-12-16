16b4e37d39bbd41305dfd0455b50044dffadc800
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-12-16 11:30:51 +0800

    Catch errors in api helper functions

--- src/scripts/apiHelpers.tsx
+++ src/scripts/apiHelpers.tsx
@@ -1,16 +1,28 @@
 import axios from "axios";
 
 export const getAllPosts = async () => {
-    const res = await axios.get("http://localhost:8000/api/posts");
-    return res.data.data;
+    try {
+        const res = await axios.get("http://localhost:8000/api/posts");
+        return res.data.data;
+    } catch (err) {
+        throw new Error("Error fetching posts");
+    }
 };
 
 export const getPostByCategory = async (category: string) => {
-    const res = await axios.get(`http://localhost:8000/api/posts/${category}`);
-    return res.data.data;
+    try {
+        const res = await axios.get(`http://localhost:8000/api/posts/${category}`);
+        return res.data.data;
+    } catch (err) {
+        throw new Error("Error fetching posts by category");
+    }
 };
 
 export const getPostByID = async (postID: string) => {
-    const res = await axios.get(`http://localhost:8000/api/posts/post/${postID}`);
-    return res.data.data;
+    try {
+        const res = await axios.get(`http://localhost:8000/api/posts/post/${postID}`);
+        return res.data.data;
+    } catch (err) {
+        throw new Error("Error fetching post by ID");
+    }
 };
